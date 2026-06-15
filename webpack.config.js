a237b9dfb39430b6d04e26345a42da0f14c5401e
jesusmna83 <jesusmna83@users.noreply.github.com>
2026-06-15 07:51:38 +0200

    Prepare GitHub Pages production build

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlProd = "https://jesusmna83.github.io/Farmacopedia/";
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
